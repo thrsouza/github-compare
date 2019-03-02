f8a46c8b7076278e47b34399b84b6e3be821ef45
Thiago de Souza <email@thiagodesouza.com.br>
2019-03-02 19:17:17 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Main from './pages/Main';
+
+import GlobalStyled from './styles/global';
+
+const App = () => (
+  <Fragment>
+    <GlobalStyled />
+    <Main />
+  </Fragment>
+);
+
+export default App;
